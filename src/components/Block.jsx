02ba08af29df400a7fe582bb3cbd35d3c3eba2cd
Yigit Yaldiz <yigityaldiz@gmail.com>
2023-08-08 20:20:48 +0300

    Bug fix in Block

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -3,20 +3,25 @@ export const Block = ({recipes}) =>{
 
 
     return(
+        <>
+        {recipes.map((recipe,i)=>( <div  id ='block' className=' bg-gray-800 flex justify-center hover:outline-blue-600 rounded-lg outline outline-2  outline-blue-500 hover:outline-4  '>
+<img key={i} id='blockImage' className=' className=" border h-[150px] w-[220  px]  rounded-lg m-2  ' src  = {recipe.url}/>
 
 
-        <div  id ='block' className=' bg-gray-800 flex justify-center hover:outline-blue-600 rounded-lg outline outline-2  outline-blue-500 hover:outline-4  '>
-            
 
-            {recipes.map((recipe,i)=>( <img key={i} id='blockImage' className=' className=" border h-[150px] w-[220  px]  rounded-lg m-2  ' src  = {recipe.url}/>))}
-           
-           
-           
-            
-       
+
+
+
+
+
+
+
+
+</div>))}
+</>
+
+
         
-  
-        
-        </div>
     )
 }
+
